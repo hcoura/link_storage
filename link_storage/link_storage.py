3703fce0a93fb9c6f5a0eb50f054bf611dfa4804
hcoura <coura.henrique@gmail.com>
2017-05-17 13:20:14 -0300

    table name

--- link_storage/link_storage.py
+++ link_storage/link_storage.py
@@ -12,38 +12,38 @@ class LinkStorage:
             url TEXT NOT NULL UNIQUE,
             tries INTEGER DEFAULT 0,
             crawled BOOLEAN DEFAULT FALSE)
-        """ % (table,))
+        """ % table)
         self.conn.commit()
 
     def new_link(self, url):
         try:
-            self.cursor.execute("INSERT INTO %s(url) VALUES (%s)", (self.table, url))
+            self.cursor.execute("INSERT INTO " + self.table + "(url) VALUES (%s)", (url,))
             self.conn.commit()
         except Exception as e:
             self.conn.rollback()
 
     def set_crawled(self, url):
         try:
-            self.cursor.execute("UPDATE %s SET crawled=TRUE  WHERE url=%s", (self.table, url))
+            self.cursor.execute("UPDATE " + self.table + " SET crawled=TRUE  WHERE url=%s", (url,))
             self.conn.commit()
         except Exception as e:
             self.conn.rollback()
 
     def get_links(self):
-        self.cursor.execute("SELECT (url) FROM %s WHERE crawled = FALSE AND tries < 3" % (self.table,))
+        self.cursor.execute("SELECT (url) FROM %s WHERE crawled = FALSE AND tries < 3" % self.table)
         rows = self.cursor.fetchall()
         rows = [r[0] for r in rows]
         return rows
 
     def set_tried(self, url_list):
         try:
-            self.cursor.execute("UPDATE %s SET tries = tries + 1  WHERE url IN %s", (tuple(url_list),))
+            self.cursor.execute("UPDATE " + self.table + " SET tries = tries + 1  WHERE url IN %s", (tuple(url_list),))
             self.conn.commit()
         except Exception as e:
             self.conn.rollback()
 
     def get_not_crawled_links(self):
-        self.cursor.execute("SELECT * FROM %s WHERE crawled = FALSE AND tries >= 3" % (self.table,))
+        self.cursor.execute("SELECT * FROM %s WHERE crawled = FALSE AND tries >= 3" % self.table)
         rows = self.cursor.fetchall()
         return rows
 
